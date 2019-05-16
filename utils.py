36d5a815cef5f6fbd81153aed95519d6a27a052e
Nikhil Barhate <nikhilbarhate99@gmail.com>
2019-05-16 13:06:28 +0530

    limited max buffer size

--- utils.py
+++ utils.py
@@ -1,14 +1,22 @@
 import numpy as np
 
 class ReplayBuffer:
-    def __init__(self):
+    def __init__(self, max_size=5e5):
         self.buffer = []
+        self.max_size = int(max_size)
+        self.size = 0
     
     def add(self, transition):
+        self.size +=1
         # transiton is tuple of (state, action, reward, next_state, done)
         self.buffer.append(transition)
     
     def sample(self, batch_size):
+      
+        if self.size > self.max_size:
+          del self.buffer[0:(self.size-self.max_size)]
+          self.size = self.max_size
+        
         indexes = np.random.randint(0, len(self.buffer), size=batch_size)
         state, action, reward, next_state, done = [], [], [], [], []
         
@@ -22,4 +30,3 @@ class ReplayBuffer:
         
         return np.array(state), np.array(action), np.array(reward), np.array(next_state), np.array(done)
     
-  
