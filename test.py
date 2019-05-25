2a1fafc7862b46789625ce5214629a52b9ba07a9
Nikhil Barhate <nikhilbarhate99@gmail.com>
2019-05-25 18:47:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,26 @@
-import gym
+import roboschool, gym
 from TD3 import TD3
 from PIL import Image
 
 def test():
-    env_name = "BipedalWalker-v2"
+    env_name = "RoboschoolWalker2d-v1"
     random_seed = 0
     n_episodes = 3
+    lr = 0.002
     max_timesteps = 2000
     render = True
     save_gif = False
     
     filename = "TD3_{}_{}".format(env_name, random_seed)
     filename += '_solved'
-    directory = "./preTrained/{}/ONE".format(env_name)
+    directory = "./preTrained/{}".format(env_name)
     
     env = gym.make(env_name)
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
     max_action = float(env.action_space.high[0])
     
-    policy = TD3(state_dim, action_dim, max_action)
+    policy = TD3(lr, state_dim, action_dim, max_action)
     
     policy.load_actor(directory, filename)
     
